0d0f14af5c2f746fdd7add369bf703ef9b4a486b
Ahmaddeveloper7 <ahmedbenyerrou1@gmail.com>
2025-05-16 19:12:43 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+print("""
+        ====================================
+            WELCOME TO THE CALCULATOR APP!
+        ====================================
+        """)
+num1=float(input('Enter a number : '))
+num2=float(input('Enter a news number : '))
+
+print("|-------------------------------------|")
+print("|-------------------------------------|")
+print("The Result is: ")
+
+print("Addition + : "+str(num1+num2))
+print("Subtraction - : "+str(num1-num2))
+print('_______________________________')
+print("Multiplication * : "+str(num1*num2))
+print("Division/ : "+str(num1/num2))
+print('_______________________________')
+print("Modulus % : "+str(num1%num2))
